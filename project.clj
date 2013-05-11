220369fa2be490db7f0e3e45d0b1c7d7b79b5e65
jchauncey <jonathan.chauncey@gmail.com>
2013-05-11 08:19:43 -0600

    License info

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clj-jenkins "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject com.rallydev/clj-jenkins "0.1.0"
+  :description "Clojure library for interacting with jenkins"
+  :url "https://github.com/RallySoftware/clj-jenkins"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [clj-http "0.6.3"]
                  [cheshire "5.1.1"]])
